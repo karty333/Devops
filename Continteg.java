36a64b0705f8e104f7149e366fe7e0d233eb16dd
karty333 <33003618+karty333@users.noreply.github.com>
2018-12-01 14:12:02 +0530

    Add files via upload

---
+++ Continteg.java
@@ -0,0 +1,14 @@
+
+public class Continteg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=5;
+int c;
+c=a+b;
+System.out.println(c);
+
+	}
+
+}
